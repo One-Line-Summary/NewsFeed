6a099537e353b7a15e04c546c5ddb70bf6799592
KIMHYEJIN <0411khj@naver.com>
2024-10-23 06:11:08 +0900

    feat: Add @Setter to User entity
    - User 엔티티에 @Setter 추가하여 필드 수정 가능하게 변경

--- src/main/java/my/newsfeed/model/entity/User.java
+++ src/main/java/my/newsfeed/model/entity/User.java
@@ -3,17 +3,19 @@ package my.newsfeed.model.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userid;
-    private String username; //유저 이름
-    private String userPassword; //유저 비밀번호
+    private long userid; // 유저 ID
+    private String username; // 유저 이름
+    private String userPassword; // 유저 비밀번호
     private String userEmail; // 유저 이메일
     private String userImageUrl; // 프로필 이미지 URL
     private String backgroundImageUrl; // 배경 이미지 URL
